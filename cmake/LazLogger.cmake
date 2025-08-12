f9351ef3622699ac74005f0e29e1b7ece02cbd86
lazzy07 <222lasantha@gmail.com>
2025-08-12 18:12:52 -0400

    Downloading and configuring LazLogger functionality added

---
+++ cmake/LazLogger.cmake
@@ -0,0 +1,21 @@
+set(LAZLOGGER_TAG v1.0.0)
+set(LAZLOGGER_DIR ${CMAKE_CURRENT_SOURCE_DIR}/vendor/laz_logger-1.0.0)
+
+if(NOT EXISTS "${LAZLOGGER_DIR}/CMakeLists.txt")
+  file(MAKE_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/vendor")
+  set(_zip "${CMAKE_BINARY_DIR}/laz_logger-${LAZLOGGER_TAG}.zip")
+  file(DOWNLOAD
+    "https://github.com/lazzy07/laz_logger/archive/refs/tags/${LAZLOGGER_TAG}.zip"
+    "${_zip}" SHOW_PROGRESS TLS_VERIFY ON)
+  execute_process(COMMAND ${CMAKE_COMMAND} -E tar xvf "${_zip}"
+                  WORKING_DIRECTORY "${CMAKE_BINARY_DIR}" RESULT_VARIABLE rc)
+  if(NOT rc EQUAL 0)
+    message(FATAL_ERROR "Failed to extract laz_logger ${LAZLOGGER_TAG}")
+  endif()
+  file(RENAME "${CMAKE_BINARY_DIR}/laz_logger-1.0.0" "${LAZLOGGER_DIR}")
+endif()
+
+set(BUILD_TESTING OFF CACHE BOOL "" FORCE)
+add_subdirectory("${LAZLOGGER_DIR}" EXCLUDE_FROM_ALL)
+
+list(FILTER ALL_CXX_FILES EXCLUDE REGEX ".*/vendor/laz_logger-1\\.0\\.0/.*")
