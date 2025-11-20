ab047ce15fad3bd10ffde23f2518b54f466bb6bc
Lasantha Senanayake <222lasantha@gmail.com>
2025-11-20 18:08:31 -0500

    Deleted old definition files
    Deleted the definition files (Actually it is a rename, check the
    previous commit)

--- libs/kasx_lib/src/data_structures/DefinitionTypes.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <cinttypes>
-
-namespace KasX::Compiler::DataStructures {
-enum DEFINITION_TYPES : std::uint8_t { TYPE_DEFINITION, ENTITY_DEFINITION, FLUENT_DEFINITION };
-}
