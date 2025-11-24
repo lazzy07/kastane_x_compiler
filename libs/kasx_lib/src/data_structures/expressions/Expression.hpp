7855c75b5c310fd2e7732373dd8ca7965b4173cf
Lasantha Senanayake <222lasantha@gmail.com>
2025-11-23 21:27:08 -0500

    Expression and Expression Types
    Expression and Expression types added for the datatypes to keep track of
    the expressions

--- libs/kasx_lib/src/data_structures/expressions/Expression.hpp
+++ libs/kasx_lib/src/data_structures/expressions/Expression.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "ExpressionTypes.hpp"
 #include "kasx/Types.hpp"
 namespace KasX::Compiler::DataStructures {
 /**
@@ -9,5 +10,6 @@ struct Expression {
   declaration_id id;      // Id of the expression
   declaration_id parent;  // Declaration/Definition id of the expression (Declaration ID)
   std::string name;       // Name of the expression
+  EXPRESSION_TYPES expression_type;
 };
 }  // namespace KasX::Compiler::DataStructures
---
+++ libs/kasx_lib/src/data_structures/expressions/ExpressionTypes.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace KasX::Compiler::DataStructures {
+/**
+ * All the expresions supported must be declared here.
+ */
+enum EXPRESSION_TYPES {
+  FLUENT,
+};
+}  // namespace KasX::Compiler::DataStructures
