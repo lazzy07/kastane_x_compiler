cecf1bb43b0c0013e0af6bf5226c68ce3ead0d1f
Lasantha Senanayake <222lasantha@gmail.com>
2025-11-24 23:23:31 -0500

    Expression constructor changes
    To create a expression, type of the expression must be passed now

--- libs/kasx_lib/src/data_structures/expressions/Expression.hpp
+++ libs/kasx_lib/src/data_structures/expressions/Expression.hpp
@@ -1,15 +1,16 @@
 #pragma once
 
+#include <string>
+
 #include "ExpressionTypes.hpp"
-#include "kasx/Types.hpp"
+
 namespace KasX::Compiler::DataStructures {
 /**
  * Main expression struct, all the expression must extend this struct
  */
 struct Expression {
-  declaration_id id;      // Id of the expression
-  declaration_id parent;  // Declaration/Definition id of the expression (Declaration ID)
-  std::string name;       // Name of the expression
+  explicit Expression(EXPRESSION_TYPES type) { this->expression_type = type; };
+  std::string name;  // Name of the expression
   EXPRESSION_TYPES expression_type;
 };
 }  // namespace KasX::Compiler::DataStructures
--- libs/kasx_lib/src/data_structures/expressions/Fluent.hpp
+++ libs/kasx_lib/src/data_structures/expressions/Fluent.hpp
@@ -4,9 +4,11 @@
 #include <vector>
 
 #include "Expression.hpp"
+#include "ExpressionTypes.hpp"
 #include "kasx/Types.hpp"
 namespace KasX::Compiler::DataStructures {
 struct Fluent : public Expression {
+  Fluent() : Expression(EXPRESSION_TYPES::FLUENT) {};
   std::vector<declaration_id> parameters;
   std::unique_ptr<Expression> value;
 };
