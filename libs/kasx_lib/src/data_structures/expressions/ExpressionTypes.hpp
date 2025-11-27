75a2918d0d92ee99aeb74f2e2a8c04f0ca6887bb
Lasantha Senanayake <222lasantha@gmail.com>
2025-11-27 10:05:28 -0500

    ExpressionTypes update
    NULL_VALUE added to the ExpressionTypes enum

--- libs/kasx_lib/src/data_structures/expressions/ExpressionTypes.hpp
+++ libs/kasx_lib/src/data_structures/expressions/ExpressionTypes.hpp
@@ -4,5 +4,5 @@ namespace KasX::Compiler::DataStructures {
 /**
  * All the expresions supported must be declared here.
  */
-enum EXPRESSION_TYPES { FLUENT, NUMBER_VALUE, UNKNOWN_VALUE };
+enum EXPRESSION_TYPES { FLUENT, NUMBER_VALUE, UNKNOWN_VALUE, NULL_VALUE };
 }  // namespace KasX::Compiler::DataStructures
