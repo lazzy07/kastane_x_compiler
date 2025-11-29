26aca5a8c16cfefd611ecda55a9437d13ec1be45
Lasantha Senanayake <222lasantha@gmail.com>
2025-11-28 23:31:21 -0500

    Expression type update

--- libs/kasx_lib/src/data_structures/expressions/ExpressionTypes.hpp
+++ libs/kasx_lib/src/data_structures/expressions/ExpressionTypes.hpp
@@ -4,5 +4,5 @@ namespace KasX::Compiler::DataStructures {
 /**
  * All the expresions supported must be declared here.
  */
-enum EXPRESSION_TYPES { FLUENT, NUMBER_VALUE, UNKNOWN_VALUE, NULL_VALUE };
+enum EXPRESSION_TYPES { FLUENT, NUMBER_VALUE, UNKNOWN_VALUE, NULL_VALUE, UNARY_OPERATION, BINARY_OPERATION };
 }  // namespace KasX::Compiler::DataStructures
