6c2fd41c3090cca0f9026f0dc664f702f219d9ba
Lasantha Senanayake <222lasantha@gmail.com>
2025-12-30 22:26:35 -0500

    Declaration cpp error fixed
    Declaration constructor changes to add fileTrace

--- libs/kasx_lib/src/data_structures/Declaration.cpp
+++ libs/kasx_lib/src/data_structures/Declaration.cpp
@@ -3,7 +3,7 @@
 * Project: KasX Compiler
 * Author: Lasantha M Senanayake
 * Date created: 2025-12-21 13:43:47
-// Date modified: 2025-12-21 13:50:57
+// Date modified: 2025-12-30 22:24:57
 * ------
 */
 
@@ -11,10 +11,11 @@
 #include <utility>
 
 #include "Log.hpp"
+#include "kasx/debug/DomainFileTrace.hpp"
 
 namespace KasX::Compiler::DataStructures::Declarations {
-Declaration::Declaration(std::string name, DECLARATION_TYPES declarationType)
-    : name(std::move(name)), declarationType(declarationType) {
+Declaration::Declaration(std::string name, DECLARATION_TYPES declarationType, Debug::DomainFileTrace fileTrace)
+    : name(std::move(name)), declarationType(declarationType), fileTrace(fileTrace) {
   CORE_TRACE("A new declaration added: {}", name);
 }
 }  // namespace KasX::Compiler::DataStructures::Declarations
