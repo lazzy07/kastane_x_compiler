0629a7adf3d97695d7c3ef33980d3070b0befb65
Lasantha Senanayake <222lasantha@gmail.com>
2026-01-03 21:56:18 -0500

    Type declaration system functionality added.

--- libs/kasx_lib/src/data_structures/Declaration.cpp
+++ libs/kasx_lib/src/data_structures/Declaration.cpp
@@ -3,19 +3,16 @@
 * Project: KasX Compiler
 * Author: Lasantha M Senanayake
 * Date created: 2025-12-21 13:43:47
-// Date modified: 2025-12-30 22:24:57
+// Date modified: 2026-01-03 21:53:12
 * ------
 */
 
 #include <kasx/data_structures/declarations/Declaration.hpp>
 #include <utility>
 
-#include "Log.hpp"
 #include "kasx/debug/DomainFileTrace.hpp"
 
 namespace KasX::Compiler::DataStructures::Declarations {
-Declaration::Declaration(std::string name, DECLARATION_TYPES declarationType, Debug::DomainFileTrace fileTrace)
-    : name(std::move(name)), declarationType(declarationType), fileTrace(fileTrace) {
-  CORE_TRACE("A new declaration added: {}", name);
-}
+Declaration::Declaration(std::string name, DECLARATION_TYPES declarationType, Debug::DomainFileTrace fileTrace, bool isMutable)
+    : name(std::move(name)), declarationType(declarationType), fileTrace(fileTrace), isMutable(isMutable) {}
 }  // namespace KasX::Compiler::DataStructures::Declarations
--- libs/kasx_lib/src/data_structures/TypeDeclaration.cpp
+++ libs/kasx_lib/src/data_structures/TypeDeclaration.cpp
@@ -3,12 +3,11 @@
 * Project: KasX Compiler
 * Author: Lasantha M Senanayake
 * Date created: 2025-12-21 13:52:44
-// Date modified: 2025-12-30 19:03:34
+// Date modified: 2026-01-03 21:43:14
 * ------
 */
 
 #include <kasx/data_structures/declarations/TypeDeclaration.hpp>
-#include <utility>
 #include <vector>
 
 #include "Log.hpp"
@@ -16,18 +15,22 @@
 #include "kasx/data_structures/declarations/EntityDeclaration.hpp"
 
 namespace KasX::Compiler::DataStructures::Declarations {
-TypeDeclaration::TypeDeclaration(std::string name, std::vector<TypeDeclaration*> parents, Debug::DomainFileTrace trace)
-    : Declaration(std::move(name), DECLARATION_TYPES::TYPE_DECL, trace), parents(std::move(parents)) {
-  CLI_TRACE("New type declaration added: {}", name);
+TypeDeclaration::TypeDeclaration(const std::string& name, const std::vector<TypeDeclaration*>& parents,
+                                 Debug::DomainFileTrace trace, bool isMutable)
+    : Declaration(name, DECLARATION_TYPES::TYPE_DECL, trace, isMutable), parents(parents) {
+  CLI_TRACE("Type declaration added: {}", this->name);
+  for (TypeDeclaration* parentDecl : this->parents) {
+    parentDecl->addChildType(this);
+  }
 }
 
-void TypeDeclaration::addNewEntityDeclaration(const EntityDeclaration& entity) {
+void TypeDeclaration::addNewEntityDeclaration(EntityDeclaration* entity) {
   this->entities.emplace_back(entity);
-  CLI_TRACE("New entity : {} added to the type: {}", entity.name, this->name);
+  CLI_TRACE("New entity : {} added to the type: {}", entity->name, this->name);
 }
 
-void TypeDeclaration::addChildType(const TypeDeclaration& child) {
+void TypeDeclaration::addChildType(TypeDeclaration* child) {
   this->children.emplace_back(child);
-  CLI_TRACE("Child type declaration {} added to the type: {}", child.name, this->name);
+  CLI_TRACE("Child type declaration {} added to the type: {}", child->name, this->name);
 }
 }  // namespace KasX::Compiler::DataStructures::Declarations
