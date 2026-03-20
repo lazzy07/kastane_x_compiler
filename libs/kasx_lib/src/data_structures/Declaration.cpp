1ce2e0779f675aa3e5576fc4ea9f0e984830426a
Lasantha Senanayake <222lasantha@gmail.com>
2026-03-20 16:08:37 -0400

    Declaration id fetch functionality and added exrta trace

--- libs/kasx_lib/src/data_structures/Declaration.cpp
+++ libs/kasx_lib/src/data_structures/Declaration.cpp
@@ -3,20 +3,25 @@
 * Project: KasX Compiler
 * Author: Lasantha M Senanayake
 * Date created: 2025-12-21 13:43:47
-// Date modified: 2026-03-20 12:40:51
+// Date modified: 2026-03-20 16:05:45
 * ------
 */
 
 #include <kasx/data_structures/declarations/Declaration.hpp>
 #include <utility>
 
+#include "../core/services/IDHandler.hpp"
 #include "Log.hpp"
 #include "kasx/debug/DomainFileTrace.hpp"
 
 namespace KasX::Compiler::DataStructures::Declarations {
 Declaration::Declaration(std::string name, DECLARATION_TYPES declarationType, Debug::DomainFileTrace fileTrace,
                          MUTABILITY mutability)
-    : Traceable(fileTrace), name(std::move(name)), declarationType(declarationType), mutability(mutability) {}
+    : Traceable(fileTrace), name(std::move(name)), declarationType(declarationType), mutability(mutability) {
+  this->id = KasX::Compiler::Core::Services::IDHandler::GetID();
+
+  CLI_TRACE("Declaration created ({}) {}", this->id, this->name);
+}
 
 Declaration::~Declaration() { CLI_TRACE("Declaration: {} got deleted", this->name); }
 
